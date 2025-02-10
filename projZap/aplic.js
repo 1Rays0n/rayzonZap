b73d87e8d1cf421756ef78abadc32baebb66a5b0
Rayson <pedropaulo@pop-os.localdomain>
2025-02-10 14:14:01 -0300

    Melhorias na performance

--- projZap/aplic.js
+++ projZap/aplic.js
@@ -11,43 +11,42 @@ wppconnect
 
 function start(client) {
   client.onMessage(async (message) => {
-
-    console.log(message);
     
     if (message.isGroupMsg || message.chatId === "status@broadcast") {
       return;
     }
-
-    const sender = message.from;
-
+    
+    const fonteContato = message.from;
+    
     // Se já estiver processando uma mensagem para esse contato, ignore
-    if (processingMessages[sender]) {
-      console.log(`Ignorando mensagem de ${sender}, ainda processando a anterior.`);
+    if (processingMessages[fonteContato]) {
+      console.log(`Ignorando mensagem de ${fonteContato}, ainda processando a anterior.`);
       return;
     }
-
+    
     // Marca como em processamento
-    processingMessages[sender] = true;
-
+    processingMessages[fonteContato] = true;
+    
     try {
-      const enviarMensagemDe = verEtapAtendimento(sender, message.notifyName, message.content);
-      const txtResposta = respostas[enviarMensagemDe].funcResposta(sender, message.notifyName);
-
+      const enviarMensagemDe = verEtapAtendimento(fonteContato, message.sender.formattedName, message.content);
+      const txtResposta = respostas[enviarMensagemDe].funcResposta(fonteContato, message.sender.formattedName);
+      
       const atraso = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
+      
       for (let index = 0; index < txtResposta.length; index++) {
         await atraso(1800);
         await client
-          .sendText(sender, txtResposta[index])
-          .then((result) => console.log('Enviado:', result))
-          .catch((erro) => console.error('Erro ao enviar:', erro));
+        .sendText(fonteContato, txtResposta[index])
+        .then((result) => console.log('Enviado:', result))
+        .catch((erro) => console.error('Erro ao enviar:', erro));
         await atraso(200);
       }
     } catch (error) {
       console.error('Erro no processamento da mensagem:', error);
     } finally {
       // Libera o processamento para novas mensagens desse contato
-      delete processingMessages[sender];
+      delete processingMessages[fonteContato];
     }
+    console.log(message);
   });
 }
