ca94d0880b5a87cdb15220abb1a36644fca9c47d
Rayson <pedropaulo@pop-os.localdomain>
2025-02-28 14:41:40 -0300

    trash

--- projZap/ObjetosZap.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // Aqui pode ter comentários
-    "chave": "valor",
-    /* 
-      Comentários de múltiplas 
-      linhas também funcionam
-    */
-    "outraChave": "outroValor"
-}; 
