f22990f589dba693741a5b4ba421cd7962db3d1d
Rayson <pedropaulo@pop-os.localdomain>
2025-02-28 14:10:36 -0300

    Nova interface

---
+++ projZap/ObjetosZap.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Aqui pode ter comentários
+    "chave": "valor",
+    /* 
+      Comentários de múltiplas 
+      linhas também funcionam
+    */
+    "outraChave": "outroValor"
+}; 
